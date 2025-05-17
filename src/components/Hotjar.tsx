9e8e05cd2479aa4a2153d671ac91b95b69d9c233
devandapaige <devandapaige@gmail.com>
2025-05-17 18:08:18 -0400

    hotjar fix

--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -3,22 +3,15 @@
 import Script from 'next/script';
 
 export default function Hotjar() {
-  const hotjarId = process.env.NEXT_PUBLIC_HOTJAR_ID;
-
-  if (!hotjarId) {
-    console.warn('Hotjar ID is not defined in environment variables');
-    return null;
-  }
-
   return (
     <Script
       id="hotjar-script"
-      strategy="afterInteractive"
+      strategy="beforeInteractive"
       dangerouslySetInnerHTML={{
         __html: `
           (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${hotjarId},hjsv:6};
+            h._hjSettings={hjid:6406931,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
