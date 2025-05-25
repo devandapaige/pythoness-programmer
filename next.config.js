7d7cdf4af933e52719b4e53f4e43867ba3bcecf4
devandapaige <devandapaige@gmail.com>
2025-05-25 17:50:26 -0400

    linked media added to next.config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   /* config options here */
